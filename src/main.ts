bce98f4dd6c12efe798423b1ef90bbc32b7cbdf7
Benjamin Dudler <benjamin.dudler@finnova.com>
2018-11-02 18:51:09 +0100

    add dependencies for lodash add mockdata for challenge

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,133 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+export interface FileNode<T> extends Node {
+  file: T
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+export interface FolderNode extends Node {
+  id?: number;
+  isRecording: boolean;
+  children: Node[];
 }
 
-// Below are examples of using TSLint errors suppression
-// Here it is suppressing missing type definitions for greeter function
-
-// tslint:disable-next-line typedef
-export async function greeter(name) {
-  // tslint:disable-next-line no-unsafe-any no-return-await
-  return await delayedHello(name, Delays.Long);
+export interface Node {
+  filename: String;
 }
+
+export interface Recording {
+  id: number;
+  name: string;
+  date: string;
+  channels: ChannelRecordingFile[];
+}
+
+export interface ChannelRecordingFile {
+  id: number;
+  channelNr: number;
+  name: string;
+  size: number;
+}
+
+export class RecordingsToFileTreeConverter {
+
+}
+
+export const recordings = [
+  {
+    id: 2342134,
+    name: 'Aufnahme 3',
+    date: 20181123,
+    recordings: [
+      {
+        id: 11,
+        channelNr: 1,
+        name: 'Gesang Robin',
+        size: 2345345
+      },
+      {
+        id: 13,
+        channelNr: 2,
+        name: 'Gesang Beni',
+        size: 243233
+      },
+      {
+        id: 15,
+        channelNr: 3,
+        name: 'Gesang Silvan',
+        size: 987987
+      }
+    ]
+  },
+  {
+    id: 566776,
+    name: 'Aufnahme 2',
+    date: 20170803,
+    recordings: [
+      {
+        id: 11,
+        channelNr: 1,
+        name: 'Gesang Robin',
+        size: 2345345
+      },
+      {
+        id: 13,
+        channelNr: 2,
+        name: 'Gesang Beni',
+        size: 243233
+      },
+      {
+        id: 15,
+        channelNr: 3,
+        name: 'Gesang Silvan',
+        size: 987987
+      }
+    ]
+  },
+  {
+    id: 566776,
+    name: 'Aufnahme 1',
+    date: 20170803,
+    recordings: [
+      {
+        id: 11,
+        channelNr: 1,
+        name: 'Gesang Robin',
+        size: 2345345
+      },
+      {
+        id: 13,
+        channelNr: 2,
+        name: 'Gesang Beni',
+        size: 243233
+      },
+      {
+        id: 15,
+        channelNr: 3,
+        name: 'Gesang Silvan',
+        size: 987987
+      }
+    ]
+  },
+  {
+    id: 7989879,
+    name: 'Aufnahme 4',
+    date: 20171013,
+    recordings: [
+      {
+        id: 11,
+        channelNr: 1,
+        name: 'Gesang Robin',
+        size: 2345345
+      },
+      {
+        id: 13,
+        channelNr: 2,
+        name: 'Gesang Beni',
+        size: 243233
+      },
+      {
+        id: 15,
+        channelNr: 3,
+        name: 'Gesang Silvan',
+        size: 987987
+      }
+    ]
+  }];
